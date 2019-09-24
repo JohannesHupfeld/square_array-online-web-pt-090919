40ea69cc45c2d16ff738ee72b983a8c9ea2764e1
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-09-24 17:34:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,4 @@
 def square_array(array)
-   squared_numbers=[]
-   array.each do|numbers|
-   squared_numbers << numbers ** 2
-  end
- return squared_numbers 
-end
-
-def square_array(array)
    array.collect do |numbers|
      numbers ** 2
    end
